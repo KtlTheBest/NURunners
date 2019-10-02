948353118038ee374a6e48a0fb5ae8ae09223327
marik <marik@localhost.localdomain>
2019-10-02 18:42:05 +0600

    I think I finished Student.java. I don't know what I've written. Just copied what was written in the decompiled code. I don't know whether Selim wanted to make it easy for us, or that he really believed that the obfuscator would change the code beyond recognition, but the code is easy to follow. I don't forget that I have a UML diagramm, but still, it seems kinda easy...

--- Student.java
+++ Student.java
@@ -1,7 +1,43 @@
+import java.awt.geom.AffineTransform;
+import java.awt.FontMetrics;
+import java.awt.Paint;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+
 public class Student extends Entity {
     public int grade;
 
-    public Student(int g){
-        grade = g;
+    public Student(String name, Vector2D position, State state, Common common){
+        super(name, position, state, common);
+        grade = 0;
+    }
+
+    @Override
+    public void draw(Graphics2D g2d){
+        Font font = g2d.getFont();
+        g2d.setFont(new Font("Sans Serif", 1, 14));
+        FontMetrics fontMetrics = g2d.getFontMetrics();
+        AffineTransform transform = g2d.getTransform();
+        g2d.translate((int)position.x, (int)position.y);
+
+        if(grade >= 100){
+            g2d.setPaint(Color.MAGENTA);
+        } else {
+            g2d.setPaint(Color.CYAN);
+        }
+
+        g2d.fillOval(-15, -15, 30, 30);
+        g2d.setPaint(Color.BLACK);
+        g2d.drawOval(-15, -15, 30, 30);
+
+        g2d.drawString(name, (int)(-fontMetrics.stringWidth(name) / 2.0) + 1, -18);
+        g2d.drawString(name, (int)(-fontMetrics.stringWidth(name) / 2.0) + 1, 6);
+
+        String stateName = state.toString();
+        g2d.drawString(stateName, (int)(-fontMetrics.stringWidth(stateName) / 2.0) + 1, 30);
+        
+        g2d.setTransform(transform);
+        g2d.setFont(font);
     }
 }
